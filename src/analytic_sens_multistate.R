bbeab07c93c0788887d4386610ed50ec5c335121
yibaizhao <yibaizhao@umass.edu>
2024-01-24 11:49:44 -0800

    Add functions for stage-specific model

---
+++ src/analytic_sens_multistate.R
@@ -0,0 +1,42 @@
+library(here)
+library(foreach)
+library(doParallel)
+
+source(here("src", "analytic_sens_multistate_funs.R"))
+
+
+test_time <- 1:15
+rate = 0.2
+start = 1
+end = 2
+lower = 0 
+upper = 0.3 
+m1 = 4
+m2 = 2
+m3 = 1
+# plot prospective sensitivity for test @ between early stage and clinical onset
+fig_prosp_sens <- function(test_time, rate, start, end, lower, upper, m1, m2, m3 = NULL){
+  ############
+  # test_time: test time at t
+  # rate: preclinical onset rate
+  # start: initial state
+  # end: end state
+  # lower: lower bound of true sensitivity
+  # upper: upper bound of true sensitivity
+  # m1: mean sojourn time for transition from early stage to clinical onset
+  # m2: mean sojourn time for transition from early stage to late stage
+  # m3: mean sojourn time for transition from late stage to clinical onset
+  ############
+  no_cores <- detectCores() - 1  # leaving one core free
+  registerDoParallel(cores=no_cores)
+  
+  # early state to clinical onset
+  prosp_sens_es_cl <- foreach(t = test_time, .combine = "rbind") %dopar% {
+    test_es_cl_integral(t, preonset_rate, m1, m2) / case_es_cl_integral(t, preonset_rate, m1, m2)
+  }
+  # early state to late stage
+  prosp_sens_es_ls <- foreach(t = test_time, .combine = "rbind") %dopar% {
+    test_es_ls_integral(t, preonset_rate, m1, m2) / case_es_ls_integral(t, preonset_rate, m1, m2)
+  }
+  
+}
---
+++ src/analytic_sens_multistate_funs.R
@@ -0,0 +1,315 @@
+# function of preclinical onset
+f <- function(u, preonset_rate){
+  preonset_rate*exp(-preonset_rate*u)
+}
+
+# function of true sensitivity in early stage
+h <- function(t,
+              sojourn_time,
+              lower,
+              upper){
+  # t: time from preclincial onset
+  beta0 <- upper
+  beta1 <- (upper - lower)/sojourn_time
+  true_sens <- beta0 + beta1 * t
+  true_sens <- pmax(lower, pmin(upper, true_sens))
+}
+
+
+# function of sojourn time
+g <- function(s, mean_sojourn_time){
+  1/mean_sojourn_time*exp(-1/mean_sojourn_time*s)
+}
+
+#########
+# Function of mean sojourn time from early stage to clinical stage given not going to late stage
+#########
+mst_es_cl_f <- function(t0, t1, t2, m1, m2){
+  # t0: time at early stage
+  # t1: time at early stage clinical onset
+  # t2: time at late stage
+  # m1: mst from early stage to clinical stage
+  # m2: mst from early stage to late stage
+  (t1 - t0) * g(s = (t1 - t0), mean_sojourn_time = m1) * g(s = (t2 - t0), mean_sojourn_time = m2)
+}
+
+mst_es_cl_integral_t1 <- function(t0, t2, m1, m2) {
+  integrate(f = mst_es_cl_f, lower = t0, upper = t2, 
+                                 t0 = t0, t2 = t2,
+                                 m1 = m1, m2 = m2)$value
+}
+# Compute the nested integral
+mst_es_cl_integral_t2 <- function(t0, m1, m2){
+  integrate(f = Vectorize(mst_es_cl_integral_t1), 
+                 lower = t0, upper = Inf,
+                 t0 = t0,
+                 m1 = m1, m2 = m2)$value
+  }
+
+# mst_es_cl_integral_t2(t0, m1, m2)
+
+#########
+# Function of mean sojourn time from early stage to late stage given not going to clinical onset
+#########
+mst_es_ls_f <- function(t0, t1, t2, m1, m2){
+  # t0: time at early stage
+  # t1: time at early stage clinical onset
+  # t2: time at late stage
+  # m1: mst from early stage to clinical stage
+  # m2: mst from early stage to late stage
+  (t2 - t0) * g(s = (t1 - t0), mean_sojourn_time = m1) * g(s = (t2 - t0), mean_sojourn_time = m2)
+}
+
+mst_es_ls_integral_t2 <- function(t0, t1, m1, m2) {
+  integrate(f = mst_es_ls_f,
+                            lower = t0, upper = t1, 
+                            t0 = t0, t1 = t1,
+                            m1 = m1, m2 = m2)$value
+}
+# Compute the nested integral
+mst_es_ls_integral_t1 <- function(t0, m1, m2){
+  integrate(f = Vectorize(mst_es_ls_integral_t2), 
+            lower = t0, upper = Inf,
+            t0 = t0,
+            m1 = m1, m2 = m2)$value
+}
+
+# mst_es_ls_integral_t1(t0, m1, m2)
+
+###############
+# Analytic function of stage-specific prospective sensitivity at time t
+###############
+# test between preclinical and preclinical onset
+f_es_cl <- function(t, t0, t1, t2, m1, m2, test = TRUE){
+  # t0: time at early stage
+  # t1: time at early stage clinical onset
+  # t2: time at late stage
+  # m1: mst from early stage to clinical stage
+  # m2: mst from early stage to late stage
+  f_joint <- g(s = (t1 - t0), mean_sojourn_time = mst_es_cl_integral_t2(t0, m1, m2)) * 
+    g(s = (t2 - t0), mean_sojourn_time = mst_es_ls_integral_t1(t0, m1, m2))
+  if(test){
+    f_joint <- f_joint * h(t = (t - t0), sojourn_time = (t1 - t0), lower = 0, upper = 0.3)
+  }
+  return(f_joint)
+}
+# integral over t1
+test_es_cl_integral_t1 <- function(t, t0, t2, m1, m2){
+  integrate(f = f_es_cl, 
+            lower = t, upper = t2, 
+            t = t, t0 = t0, t2 = t2,
+            m1 = m1, m2 = m2)$value
+}
+# integral over t2
+test_es_cl_integral_t2 <- function(t, t0, m1, m2){
+  integrate(f = Vectorize(test_es_cl_integral_t1), 
+            lower = t, upper = Inf,
+            t = t, t0 = t0,
+            m1 = m1, m2 = m2)$value
+}
+# integral over t0
+f_es_cl_integral_t0 <- function(t, t0, preonset_rate, m1, m2){
+  f(u = t0, preonset_rate) * test_es_cl_integral_t2(t, t0, m1, m2)
+}
+
+test_es_cl_integral <- function(t, preonset_rate, m1, m2){
+  integrate(f = Vectorize(f_es_cl_integral_t0), 
+            lower = 0, upper = t,
+            t = t,
+            preonset_rate = preonset_rate,
+            m1 = m1, m2 = m2)$value
+}
+# test_es_cl_integral(t, preonset_rate, m1, m2)
+
+
+# test between preclinical and late stage
+f_es_ls <- function(t, t0, t1, t2, m1, m2, test = TRUE){
+  # t0: time at early stage
+  # t1: time at early stage clinical onset
+  # t2: time at late stage
+  # m1: mst from early stage to clinical stage
+  # m2: mst from early stage to late stage
+  f_joint <- g(s = (t1 - t0), mean_sojourn_time = mst_es_cl_integral_t2(t0, m1, m2)) * 
+    g(s = (t2 - t0), mean_sojourn_time = mst_es_ls_integral_t1(t0, m1, m2))
+  if(test){
+    f_joint <- f_joint * h(t = (t - t0), sojourn_time = (t2 - t0), lower = 0, upper = 0.8)
+  }
+  return(f_joint)
+}
+# integral over t2
+test_es_ls_integral_t2 <- function(t, t0, t1, m1, m2){
+  integrate(f = f_es_ls, 
+            lower = t, upper = t1, 
+            t = t, t0 = t0, t1 = t1,
+            m1 = m1, m2 = m2)$value
+}
+# integral over t1
+test_es_ls_integral_t1 <- function(t, t0, m1, m2){
+  integrate(f = Vectorize(test_es_ls_integral_t2), 
+            lower = t, upper = Inf,
+            t = t, t0 = t0,
+            m1 = m1, m2 = m2)$value
+}
+# integral over t0
+f_es_ls_integral_t0 <- function(t, t0, preonset_rate, m1, m2){
+  f(u = t0, preonset_rate) * test_es_ls_integral_t1(t, t0, m1, m2)
+}
+
+test_es_ls_integral <- function(t, preonset_rate, m1, m2){
+  integrate(f = Vectorize(f_es_ls_integral_t0), 
+            lower = 0, upper = t,
+            t = t,
+            preonset_rate = preonset_rate,
+            m1 = m1, m2 = m2)$value
+}
+# test_es_ls_integral(t, preonset_rate, m1, m2)
+
+
+# test between late stage and clinical stage
+f_ls_cl <- function(t, t0, t1, t2, t3, m1, m2, m3, test = TRUE){
+  # t0: time at early stage
+  # t1: time at early stage clinical onset
+  # t2: time at late stage
+  # m1: mst from early stage to clinical stage
+  # m2: mst from early stage to late stage
+  f_joint <- g(s = (t1 - t0), mean_sojourn_time = mst_es_cl_integral_t2(t0, m1, m2)) * 
+    g(s = (t2 - t0), mean_sojourn_time = mst_es_ls_integral_t1(t0, m1, m2)) *
+    g(s = (t3 - t2), mean_sojourn_time = m3)
+  if(test){
+    f_joint <- f_joint * 0.8
+  }
+  return(f_joint)
+  
+}
+# integral over t1
+test_ls_cl_integral_t1 <- function(t, t0, t2, t3, m1, m2, m3){
+  integrate(f = f_ls_cl, 
+            lower = t2, upper = Inf, 
+            t = t, t0 = t0, t2 = t2, t3 = t3,
+            m1 = m1, m2 = m2, m3 = m3)$value
+}
+# integral over t2
+test_ls_cl_integral_t2 <- function(t, t0, t3, m1, m2, m3){
+  integrate(f = Vectorize(test_ls_cl_integral_t1), 
+            lower = t0, upper = t3,
+            t = t, t0 = t0, t3 = t3, 
+            m1 = m1, m2 = m2, m3 = m3)$value
+}
+# integral over t3
+test_ls_cl_integral_t3 <- function(t, t0, m1, m2, m3){
+  integrate(f = Vectorize(test_ls_cl_integral_t2), 
+            lower = t, upper = Inf,
+            t = t, t0 = t0, 
+            m1 = m1, m2 = m2, m3 = m3)$value
+}
+# integral over t0
+f_ls_cl_integral_t0 <- function(t, t0, preonset_rate, m1, m2, m3){
+  f(u = t0, preonset_rate) * test_ls_cl_integral_t3(t, t0, m1, m2, m3)
+}
+
+test_ls_cl_integral <- function(t, preonset_rate, m1, m2, m3){
+  integrate(f = Vectorize(f_ls_cl_integral_t0), 
+            lower = 0, upper = t,
+            t = t,
+            preonset_rate = preonset_rate,
+            m1 = m1, m2 = m2, m3 = m3)$value
+}
+# test_ls_cl_integral(t, preonset_rate, m1, m2, m3)
+
+# cases between preclinical onset and clinical onset
+# integral over t1
+case_es_cl_integral_t1 <- function(t, t0, t2, m1, m2){
+  integrate(f = f_es_cl, 
+            lower = t, upper = t2, 
+            t = t, t0 = t0, t2 = t2,
+            m1 = m1, m2 = m2,
+            test = FALSE)$value
+}
+# integral over t2
+case_es_cl_integral_t2 <- function(t, t0, m1, m2){
+  integrate(f = Vectorize(case_es_cl_integral_t1), 
+            lower = t, upper = Inf,
+            t = t, t0 = t0,
+            m1 = m1, m2 = m2)$value
+}
+# integral over t0
+f_case_es_cl_integral_t0 <- function(t, t0, preonset_rate, m1, m2){
+  f(u = t0, preonset_rate) * case_es_cl_integral_t2(t, t0, m1, m2)
+}
+
+case_es_cl_integral <- function(t, preonset_rate, m1, m2){
+  integrate(f = Vectorize(f_case_es_cl_integral_t0), 
+            lower = 0, upper = t,
+            t = t,
+            preonset_rate = preonset_rate,
+            m1 = m1, m2 = m2)$value
+}
+# case_es_cl_integral(t, preonset_rate, m1, m2)
+
+# case between preclinical and late stage
+# integral over t2
+case_es_ls_integral_t2 <- function(t, t0, t1, m1, m2){
+  integrate(f = f_es_ls, 
+            lower = t, upper = t1, 
+            t = t, t0 = t0, t1 = t1,
+            m1 = m1, m2 = m2,
+            test = FALSE)$value
+}
+# integral over t1
+case_es_ls_integral_t1 <- function(t, t0, m1, m2){
+  integrate(f = Vectorize(case_es_ls_integral_t2), 
+            lower = t, upper = Inf,
+            t = t, t0 = t0,
+            m1 = m1, m2 = m2)$value
+}
+# integral over t0
+f_case_es_ls_integral_t0 <- function(t, t0, preonset_rate, m1, m2){
+  f(u = t0, preonset_rate) * case_es_ls_integral_t1(t, t0, m1, m2)
+}
+
+case_es_ls_integral <- function(t, preonset_rate, m1, m2){
+  integrate(f = Vectorize(f_case_es_ls_integral_t0), 
+            lower = 0, upper = t,
+            t = t,
+            preonset_rate = preonset_rate,
+            m1 = m1, m2 = m2)$value
+}
+# case_es_ls_integral(t, preonset_rate, m1, m2)
+
+# case between late stage and clinical stage
+# integral over t1
+case_ls_cl_integral_t1 <- function(t, t0, t2, t3, m1, m2, m3){
+  integrate(f = f_ls_cl, 
+            lower = t2, upper = Inf, 
+            t = t, t0 = t0, t2 = t2, t3 = t3,
+            m1 = m1, m2 = m2, m3 = m3,
+            test = FALSE)$value
+}
+# integral over t2
+case_ls_cl_integral_t2 <- function(t, t0, t3, m1, m2, m3){
+  integrate(f = Vectorize(case_ls_cl_integral_t1), 
+            lower = t0, upper = t3,
+            t = t, t0 = t0, t3 = t3, 
+            m1 = m1, m2 = m2, m3 = m3)$value
+}
+# integral over t3
+case_ls_cl_integral_t3 <- function(t, t0, m1, m2, m3){
+  integrate(f = Vectorize(case_ls_cl_integral_t2), 
+            lower = t, upper = Inf,
+            t = t, t0 = t0, 
+            m1 = m1, m2 = m2, m3 = m3)$value
+}
+# integral over t0
+f_case_ls_cl_integral_t0 <- function(t, t0, preonset_rate, m1, m2, m3){
+  f(u = t0, preonset_rate) * case_ls_cl_integral_t3(t, t0, m1, m2, m3)
+}
+
+case_ls_cl_integral <- function(t, preonset_rate, m1, m2, m3){
+  integrate(f = Vectorize(f_case_ls_cl_integral_t0), 
+            lower = 0, upper = t,
+            t = t,
+            preonset_rate = preonset_rate,
+            m1 = m1, m2 = m2, m3 = m3)$value
+}
+# case_ls_cl_integral(t, preonset_rate, m1, m2)
+
